8cb4502bd60a3ce668635502d3081b473737b3c1
Callum Rogers <callumr@palantir.com>
2014-11-16 21:31:14 +0000

    Structured tests wiht describe blocks

--- test-src/Test.hs
+++ test-src/Test.hs
@@ -15,29 +15,32 @@ test = hspec $ do
     let three = Const $ Num 3
     let four = Const $ Num 4
 
-    it "true should have type BoolTy" $ do
-      true `shouldHaveType` BoolTy
+    describe "Constants:" $ do
+      it "true should have type BoolTy" $ do
+        true `shouldHaveType` BoolTy
 
-    it "false should have type BoolTy" $ do
-      false `shouldHaveType` BoolTy
+      it "false should have type BoolTy" $ do
+        false `shouldHaveType` BoolTy
 
-    it "3 should have type IntTy" $ do
-      three `shouldHaveType` NumTy
+      it "3 should have type IntTy" $ do
+        three `shouldHaveType` NumTy
 
-    it "an if statement where the then and else branch are both BoolTys should be of type BoolTy" $ do
-      If true true false `shouldHaveType` BoolTy
+    describe "If Statements:" $ do
+      it "when the then and else branches are both BoolTys should be of type BoolTy" $ do
+        If true true false `shouldHaveType` BoolTy
 
-    it "an if statement where the then and else branch are both NumTys should be of type NumTy" $ do
-      If true three four `shouldHaveType` NumTy
+      it "when the then and else branches are both NumTys should be of type NumTy" $ do
+        If true three four `shouldHaveType` NumTy
 
-    it "an if statement where the then and else branch are different should fail" $ do
-      expectToFailTypeInference $ If true true four
+      it "when the then and else branches are different should fail" $ do
+        expectToFailTypeInference $ If true true four
 
-    it "an if statement where the condition is not of type BoolTy should fail" $ do
-      expectToFailTypeInference $ If three true true
+      it "when the condition is not of type BoolTy should fail" $ do
+        expectToFailTypeInference $ If three true true
 
-    it "an abstraction returning a BoolTy should have type BoolTy -> BoolTy" $ do
-      Abs "x" true `shouldHaveType` (BoolTy :-> BoolTy)
+    describe "Abstractions" $ do
+      it "when the body is of type BoolTy it should have type BoolTy -> BoolTy" $ do
+        Abs "x" true `shouldHaveType` (BoolTy :-> BoolTy)
 
-    it "an abstraction returning an NumTy should have type BoolTy -> NumTy" $ do
-      Abs "x" three `shouldHaveType` (BoolTy :-> NumTy)
+      it "when the body is of type NumTy it should have type BoolTy -> NumTy" $ do
+        Abs "x" three `shouldHaveType` (BoolTy :-> NumTy)
