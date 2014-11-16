cf8b64c0bd8ff1c2f48dbd10ee4329d6b70db69a
Callum Rogers <callumr@palantir.com>
2014-11-16 20:58:20 +0000

    Refactor to shouldHaveType

--- test-src/Test.hs
+++ test-src/Test.hs
@@ -7,22 +7,27 @@ import Typecheck
 test :: IO ()
 test = hspec $ do
   describe "Typecheck:" $ do
+    let shouldHaveType e t = typeOf e `shouldBe` t
+
     let true = Const $ Boolean True
     let false = Const $ Boolean False
     let three = Const $ Num 3
     let four = Const $ Num 4
 
     it "true should have type BoolTy" $ do
-      typeOf true `shouldBe` BoolTy
+      true `shouldHaveType` BoolTy
 
     it "false should have type BoolTy" $ do
-      typeOf false `shouldBe` BoolTy
+      false `shouldHaveType` BoolTy
 
     it "3 should have type IntTy" $ do
-      typeOf three `shouldBe` NumTy
+      three `shouldHaveType` NumTy
 
     it "an if statement where the then and else branch are both BoolTys should be of type BoolTy" $ do
-      typeOf (If true true false) `shouldBe` BoolTy
+      If true true false `shouldHaveType` BoolTy
 
     it "an if statement where the then and else branch are both NumTys should be of type NumTy" $ do
-      typeOf (If true three four) `shouldBe` NumTy
+      If true three four `shouldHaveType` NumTy
+
+    {-it "an if statement where the then and else branch are different should fail" $ do
+      typeOf (If true three four) `shouldBe` None-}
